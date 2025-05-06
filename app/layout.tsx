cc8f53f739ac2a95f52e65a3c772d61e24b15a25
kofolmarko <kofolmarko00@gmail.com>
2025-05-06 11:59:18 +0200

    trying fix 2, whole html dark class

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="dark">
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
